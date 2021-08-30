82dfaf205b2267549ce6b57ef526b8d90c588e15
toohottostop <polezaevkolka@gmail.com>
2021-08-31 00:58:01 +0500

    make test with TestCase

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,20 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('localhost:8000')
 
-assert 'Django' in browser.page_source
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_get_title(self):
+        self.browser.get('localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Stop testing!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')

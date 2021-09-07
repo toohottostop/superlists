1e148df0f6bb8c158478ff7124aa317b8748bbe9
toohottostop <polezaevkolka@gmail.com>
2021-09-08 00:27:54 +0500

    func test check possibility to enter list element

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
+import time
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,8 +13,21 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_get_title(self):
         self.browser.get('localhost:8000')
-
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertIn(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.test == '1: Buy peacock feathers') for row in rows)
+
         self.fail('Stop testing!')
 
 

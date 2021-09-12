7018c0fa248e223eb1391b563269d60ac331a9b1
toohottostop <polezaevkolka@gmail.com>
2021-09-12 18:06:30 +0500

    update tests using explicit time.sleep add test to check multiple users trying create there own lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,22 +2,33 @@ import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 
 
 class NewVisitorTest(LiveServerTestCase):
+    MAX_WAIT = 10
+
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
 
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def _check_rows_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_rows_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as exc:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise exc
+                time.sleep(.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
 
@@ -29,14 +40,43 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self._check_rows_in_list_table('1: Buy peacock feathers')
+        self.wait_rows_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Make peacock feather bait')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self._check_rows_in_list_table('1: Buy peacock feathers')
-        self._check_rows_in_list_table('2: Make peacock feather bait')
+        self.wait_rows_in_list_table('1: Buy peacock feathers')
+        self.wait_rows_in_list_table('2: Make peacock feather bait')
 
         self.fail('Stop testing!')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_rows_in_list_table('1: Buy peacock feathers')
+
+        user_url = self.browser.current_url
+        self.assertRegex(user_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Make peacock feather bait', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_rows_in_list_table('1: Buy milk')
+
+        another_user_url = self.browser.current_url
+        self.assertRegex(another_user_url, '/lists/.+')
+        self.assertNotEqual(another_user_url, user_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
